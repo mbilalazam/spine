1bfd17fb1ed9cf9c12e9a9bc7f0ed0435b8da06c
Muhammad Bilal Azam <124085891+mbilalazam@users.noreply.github.com>
2024-10-09 11:25:39 -0500

    Create files.py

---
+++ instructions/files.py
@@ -0,0 +1,14 @@
+# Large Ml-Reco LArCV files
+/lus/eagle/clone/g2/projects/Nu_Novel/DUNE/DUNE_2x2_RHC_MLreco_MR6
+Training: train_00_09.root, train_10_11.root
+Test: test.root
+ 
+ 
+# Small files/weights from Spine Workshop
+/lus/eagle/clone/g2/projects/Nu_Novel/Tutorials/spine_workshop_2024/data_samples
+Small_LArCV_files: 2x2_small.root  generic_small.root  icarus_small.root  sbnd_small.root
+weights: 2x2_snapshot-3999.ckpt  generic_snapshot-4999.ckpt  icarus_snapshot-7999.ckpt  sbnd_snapshot-1999.ckpt
+
+# Some Directories
+/lus/eagle/clone/g2/projects/Nu_Novel/
+/lus/grand/projects/Nu_Novel
